a96c38aa348cd54d3643a492fb6dec4a8e4a0dfb
Youmat64 <fennour65@gmail.com>
2025-05-19 15:44:17 +0200

    Add files via upload

---
+++ infopelis.js
@@ -0,0 +1,27 @@
+// Array d’objectes de pel·lícules
+const pelicules = [
+  {
+    titol: "El Viatge d’en Chihiro",
+    director: "Hayao Miyazaki",
+    any: 2001,
+    genere: "Animació"
+  },
+  {
+    titol: "Paràsits",
+    director: "Bong Joon-ho",
+    any: 2019,
+    genere: "Drama"
+  },
+  {
+    titol: "Inception",
+    director: "Christopher Nolan",
+    any: 2010,
+    genere: "Ciència-ficció"
+  },
+  {
+    titol: "Call Me by Your Name",
+    director: "Luca Guadagnino",
+    any: 2017,
+    genere: "Romàntic"
+  }
+];
